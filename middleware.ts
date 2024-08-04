22f271f379cbb9716348dddd2a7e647cd6f0b263
Ayush Chauhan <ayush.chuahan.22cse@bmu.edu.in>
2024-08-04 22:17:44 +0530

    Fixed page

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/','/api/webhooks(.*)','/categories','/blog(.*)','/search(.*)']);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/','/api/webhooks(.*)','/category(.*)','/blog(.*)','/search(.*)']);
 
 export default clerkMiddleware((auth, request) => {
     if(!isPublicRoute(request)) {
